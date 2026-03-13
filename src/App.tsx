3f2a8aab523382e17f24b6cd3b483a4663744393
Ranulfo01 <ranulfober_100@hotmail.com>
2026-03-12 21:07:17 -0600

    propiedaddes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import PropertyDetail from "./pages/PropertyDetail";
+import Properties from "./pages/Properties"
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/property/:id" element={<PropertyDetail />} />
+        <Route path="/properties" element={<Properties />}
+/>
       </Routes>
 
     </div>
---
+++ src/pages/Properties.tsx
@@ -0,0 +1,81 @@
+import { useState } from "react"
+import { properties } from "../data/properties"
+import PropertyCard from "../components/PropertyCard"
+
+    export default function Properties(){
+
+        const [city,setCity] = useState("")
+        const [minPrice,setMinPrice] = useState("")
+        const [bedrooms,setBedrooms] = useState("")
+
+        const filteredProperties = properties.filter((property)=>{
+
+            return(
+
+                (city === "" || property.location.includes(city)) && 
+                (minPrice === "" || property.price >= Number(minPrice)) &&
+                (bedrooms === "" || property.rooms === Number(bedrooms))    
+
+                )
+
+        })
+
+            return(
+
+            <div className="max-w-7xl mx-auto p-6">
+                <h1 className="text-3xl font-bold mb-6">
+                     Todas las propiedades
+                </h1>
+
+{/* FILTROS */}
+
+            <div className="grid md:grid-cols-3 gap-4 mb-8">
+
+             <input
+                type="text"
+                placeholder="Ciudad"
+                className="border p-3 rounded text-black"
+                value={city}
+                onChange={(e)=>setCity(e.target.value)}
+            />
+
+            <input
+                type="number"
+                placeholder="Precio mínimo"
+                className="border p-3 rounded text-black"
+                value={minPrice}
+             onChange={(e)=>setMinPrice(e.target.value)}
+            />
+
+            <select
+                className="border p-3 rounded text-black"
+                value={bedrooms}
+                onChange={(e)=>setBedrooms(e.target.value)}
+            >
+
+            <option value="">Habitaciones</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4+</option>
+
+        </select>
+
+        </div>
+
+{/* GRID */}
+
+            <div className="grid sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6 items-center">
+                {filteredProperties.map((property)=>(
+                    <PropertyCard
+                        key={property.id}
+                        property={property}
+            />
+        ))}
+
+    </div>
+
+</div>
+
+)
+}
